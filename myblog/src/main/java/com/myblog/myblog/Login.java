fb822a4c5576ef710a7f78d120ac1afd91050eec
Swarup <jagritipal19@gmail.com>
2024-01-15 14:20:30 +0530

    Created Get Post Features

---
+++ myblog/src/main/java/com/myblog/myblog/Login.java
@@ -0,0 +1,20 @@
+//package com.myblog.myblog;
+//
+//public class Login {
+//
+//    private String username;
+//    private String Password;
+//
+//    public Login(String username, String password) {
+//        this.username = username;
+//        Password = password;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public String getPassword() {
+//        return Password;
+//    }
+//}
